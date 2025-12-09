4ea6ce504417dc04f28ffc7841dc94520fd4ce3d
yasin-arafat26 <mdyasina001@gmail.com>
2025-12-09 19:18:27 +0600

    Add variable file

--- variable.dart
+++ variable.dart
@@ -1,1 +1,13 @@
-# dart-15days
+void main() {
+  var name = "Hira";
+  int age = 20;
+  double height = 5.5;
+  final country = "Bangladesh";
+  String? nickname;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Country: $country");
+  print("Nickname: $nickname");
+}
